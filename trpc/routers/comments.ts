482360b86d14b8696b1a1547c9db099c148d8b78
Addy Ireland <addy.t.ireland@gmail.com>
2025-08-25 19:22:50 -0400

    feat: comment after

--- trpc/routers/comments.ts
+++ trpc/routers/comments.ts
@@ -1,15 +1,16 @@
-import { z } from "zod";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { z } from "zod"
+import { baseProcedure, createTRPCRouter } from "../init"
 import {
   db,
   commentsTable,
   usersTable,
   definitionsTable,
-  chatsTable,
-} from "@yamz/db";
-import { asc, eq, getTableColumns } from "drizzle-orm";
-import { authenticatedProcedure } from "../procedures";
-import { reviseDefinition } from "@/lib/apis/ollama";
+  chatsTable
+} from "@yamz/db"
+import { asc, eq, getTableColumns } from "drizzle-orm"
+import { authenticatedProcedure } from "../procedures"
+import { reviseDefinition } from "@/lib/apis/ollama"
+import { after } from "next/server"
 
 export const commentsRouter = createTRPCRouter({
   get: baseProcedure.input(z.number()).query(async ({ input: id }) => {
@@ -17,15 +18,15 @@ export const commentsRouter = createTRPCRouter({
       .select({
         ...getTableColumns(commentsTable),
         author: {
-          name: usersTable.name,
-        },
+          name: usersTable.name
+        }
       })
       .from(commentsTable)
       .where(eq(commentsTable.definitionId, id))
       .innerJoin(usersTable, eq(commentsTable.userId, usersTable.id))
-      .orderBy(asc(commentsTable.createdAt));
+      .orderBy(asc(commentsTable.createdAt))
 
-    return comments;
+    return comments
   }),
   create: authenticatedProcedure
     .input(z.object({ id: z.number(), comment: z.string().nonempty() }))
@@ -36,38 +37,39 @@ export const commentsRouter = createTRPCRouter({
           .values({
             definitionId: id,
             userId,
-            message: comment,
+            message: comment
           })
-          .returning();
+          .returning()
 
         // fetch some info about this term
         const [definition] = await tx
           .select({
             isAi: usersTable.isAi,
             termId: definitionsTable.termId,
-            id: definitionsTable.id,
+            id: definitionsTable.id
           })
           .from(definitionsTable)
           .where(eq(definitionsTable.id, id))
           .innerJoin(usersTable, eq(usersTable.id, definitionsTable.authorId))
-          .limit(1);
+          .limit(1)
 
         // if ai made, create feedback chat
         if (definition.isAi) {
-
           await tx.insert(chatsTable).values({
             role: "user",
             message: `<feedback>\n${comment}`,
-            termId: definition.termId,
-          });
+            termId: definition.termId
+          })
 
-          // Revise our definition with the new comment
-          reviseDefinition(definition.termId)
+          after(() =>
+            // Revise our definition with the new comment
+            reviseDefinition(definition.termId)
+          )
         }
 
-        return insertedComment;
-      });
+        return insertedComment
+      })
 
-      return insertedComment;
-    }),
-});
+      return insertedComment
+    })
+})
